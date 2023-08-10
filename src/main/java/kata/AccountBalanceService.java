0b6e7956faeb40b5b006b1026000cb1f25fb5111
Pablo Tapia <pablo.tapia@inviu.com.ar>
2023-08-10 12:13:38 -0300

    commit

---
+++ src/main/java/kata/AccountBalanceService.java
@@ -0,0 +1,15 @@
+package kata;
+public class AccountBalanceService {
+
+    public static void executeCashIn(Object amount, Object account){
+        return;
+    }
+
+    public static void executeCashOut(Object amount, Object sourceAccount){
+        return;
+    }
+
+    public static void executeCashTransfer(Object amount, Object sourceAccount, Object destinationAccount){
+        return;
+    }
+}
